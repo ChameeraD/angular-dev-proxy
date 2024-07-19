751467a22210c6ba7b6f9be9a24d2f330570df2e
ChameeraD <chameeradulanga87@gmail.com>
2024-07-19 22:42:50 +0530

    dev proxy creation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DataService } from './data.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-dev-proxy';
+
+  data: any;
+
+  constructor(private corsService: DataService) {}
+
+  ngOnInit(): void {
+    this.corsService.getData().subscribe(
+      data => {
+        console.log(data);
+        this.data = data;
+      },
+      (error) => {
+        console.error('Error fetching data:', error);
+      }
+    );
+  }
+
 }
